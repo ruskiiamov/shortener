9c88246b96e1243efb1168ba311bded0abc45299
ruslan-kiiamov <xkijam@gmail.com>
2023-04-18 10:21:08 +0300

    iter24

--- internal/server/userauth_mock.go
+++ internal/server/userauth_mock.go
@@ -6,11 +6,13 @@ type mockedUserAuth struct {
 	mock.Mock
 }
 
+// CreateUser is mocked method.
 func (m *mockedUserAuth) CreateUser() (userID, token string, err error) {
 	args := m.Called()
 	return args.String(0), args.String(1), args.Error(2)
 }
 
+// GetUserID is mocked method.
 func (m *mockedUserAuth) GetUserID(token string) (string, error) {
 	args := m.Called(token)
 	return args.String(0), args.Error(1)
